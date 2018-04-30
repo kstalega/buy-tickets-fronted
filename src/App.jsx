9b56bc51dc28b5b11d7479c209e4b996295f8808
Konrad Stalega <konradstalega@gmail.com>
2018-04-30 12:35:35 +0200

    Using react material ui provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,25 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Home from './scenes/Home';
 import Login from './scenes/Login';
 
 const App = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+    <MuiThemeProvider>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
 
-      <hr />
+        <hr />
 
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-    </div>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+      </div>
+    </MuiThemeProvider>
   </Router>
 );
 export default App;
