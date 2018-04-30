c5b6cef9c308dc11b2ecae83dfdf2e391b86aa31
Konrad Stalega <konradstalega@gmail.com>
2018-04-30 12:08:41 +0200

    Basic routing structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Home from './scenes/Home';
+import Login from './scenes/Login';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
 
-export default App;
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
+    </div>
+  </Router>
+)
+export default App
---
+++ src/scenes/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login = () =>
+  (
+    <div className="container">
+      <h1>Login</h1>
+    </div>
+  );
+
+export default Login;
