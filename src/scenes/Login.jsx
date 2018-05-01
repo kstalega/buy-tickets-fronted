9743bd2feba6ee233492cba36cd5362ab2048f74
Konrad Stalega <konradstalega@gmail.com>
2018-05-01 13:39:27 +0200

    Login form boiler plate

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
 
 const Login = () =>
   (
     <div className="container">
-      <h1>Login</h1>
+      <Paper>
+        <h1>Login</h1>
+        <div>
+          <TextField floatingLabelText="Login" />
+        </div>
+        <div>
+          <TextField floatingLabelText="Password" type="password" />
+        </div>
+      </Paper>
     </div>
   );
 
