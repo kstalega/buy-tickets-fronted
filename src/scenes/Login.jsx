8df7882d2ee4a9225505bc718b36ed7644165db1
Konrad Stalega <konradstalega@gmail.com>
2018-05-01 15:56:31 +0200

    Structure of login form is almost finished

--- src/scenes/Login.jsx
+++ src/scenes/Login.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import './Login.css';
 
 const Login = () =>
   (
     <div className="container">
       <Paper>
         <h1>Login</h1>
-        <div>
+        <div className="formEntity">
           <TextField label="Login" />
         </div>
-        <div>
+        <div className="formEntity">
           <TextField label="Password" type="password" />
+        </div>
+        <div className="formEntity">
+          <Button variant="raised" color="primary">
+            Sign in
+          </Button>
         </div>
       </Paper>
     </div>
