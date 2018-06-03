27addf75988762d1356f43958d371839d8e11ef3
Konrad Stalęga <konradstalega@gmail.com>
2018-06-03 15:01:11 +0200

    Working getting logged state from global state of application.

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { 
   Route,
   Redirect,
+  withRouter,
 } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { isLoggedIn, hasUserEnoughPermissionLevel } from '../services/AuthService';
 
 function RedirectToLogin(props) {
@@ -28,18 +30,40 @@ function notEnoughPermissionLevel() {
   );
 }
 
-const PrivateRoute = ({ component: Component, neededpermission: neededPermission = 0, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) => {
-      if (isLoggedIn() && hasUserEnoughPermissionLevel(neededPermission)) {
-        return (<Component {...props} />);
-      } else if (isLoggedIn()) {
-        return notEnoughPermissionLevel();
-      }    
-      return RedirectToLogin(props);
-    }}
-  />
-);
+class PrivateRoute extends React.Component {
+  render() {
+    const { 
+      component: Component, 
+      neededpermission: neededPermission = 0,
+      logged,
+      ...rest 
+    } = this.props;
 
-export default PrivateRoute;
+    return (
+      <Route
+        {...rest}
+        render={(props) => {
+          if (logged && hasUserEnoughPermissionLevel(neededPermission)) {
+            return (<Component {...props} />);
+          } else if (isLoggedIn()) {
+            return notEnoughPermissionLevel();
+          }    
+          return RedirectToLogin(props);
+        }}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const { logged } = state.auth;
+
+  return {
+    logged,
+  };
+};
+
+export default withRouter(connect(
+  mapStateToProps,
+  {},
+)(PrivateRoute));
