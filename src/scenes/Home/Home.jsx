82f3ba4d01fc5d62aa6c29ad87d39ffa6425812b
Konrad Stalęga <konradstalega@gmail.com>
2018-06-03 10:32:36 +0200

    Better looking homepage.

--- src/scenes/Home/Home.jsx
+++ src/scenes/Home/Home.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () =>
   (
-    <div className="col-sm-12">
-      <div className="panel panel-default">
-        <div className="panel-body">
-          <h1>Home!</h1>
+    <section>
+      <div className="col-sm-12">
+        <div className="panel panel-default">
+          <div className="panel-body">
+            <h1>Home!</h1>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="col-sm-12">
+        <div className="jumbotron text-center">
+          <h2>Unforgettable Events</h2>
+          <Link className="btn btn-lg btn-success" to="/events/show">Check them out!</Link>
+        </div>
+      </div>
+    </section>
   );
 
 export default Home;
