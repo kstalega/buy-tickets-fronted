fc800bf7c7082301be0c1387b9b526b05229cea8
Konrad Stalęga <konradstalega@gmail.com>
2018-06-03 18:46:33 +0200

    Better UI for choosing tickets for event.

--- src/components/Tickets/Ticket/Ticket.jsx
+++ src/components/Tickets/Ticket/Ticket.jsx
@@ -9,20 +9,48 @@ function classBasedOnStatus(status) {
 }
 
 class Ticket extends React.Component {
+  renderTicketType() {
+    if (this.props.ticket.type.toLowerCase() === 'premium') {
+      return (
+        <span className="label label-warning">{this.props.ticket.type.toLowerCase()}</span>
+      )
+    }
+
+    return (
+      <span className="label label-default">
+        {this.props.ticket.type.toLowerCase()}
+      </span>
+    )
+  }
+
+  renderBuyButton() {
+    const { id: ticketID, status, eventID } = this.props.ticket;
+    if (status.toLowerCase() === 'occupied') {
+      return (null);
+    }
+
+    return (
+      <button
+        type="button"
+        className="btn btn-success pull-right"
+        onClick={() => this.props.ActionEventBuyTicket(getUserID(), ticketID, eventID)}
+      >
+        Buy
+      </button>
+    );
+  }
+
   render() {
     const { id: ticketID, status, type, eventID } = this.props.ticket;
     return (
-      <li className={ 'list-group-item ticket col-xs-3 ' + classBasedOnStatus(status) }>
-        <div>
-          Ticket ID: {ticketID}, Status: {status}, type: {type}
+      <li className={ 'ticket-container ' + classBasedOnStatus(status) }>
+        <div className="ticket">
+          <div>
+            <span className="id">ID: #{ticketID}</span><br />
+            {this.renderTicketType()}
+          </div>
+          {this.renderBuyButton()}
         </div>
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={() => this.props.ActionEventBuyTicket(getUserID(), ticketID, eventID)}
-        >
-          Buy
-        </button>
       </li>
     );
   }
--- src/components/Tickets/TicketsList/TicketsList.jsx
+++ src/components/Tickets/TicketsList/TicketsList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Ticket from '../Ticket/Ticket';
+import './TicketList.css';
 
 class TicketsList extends React.Component {
   render() {
     return (
       <div>
         <h4>List of tickets</h4>
-        <ul className="list-group">
+        <ul className="ticket-list">
         {
           this.props.tickets.map((ticket) => {
             return (<Ticket ticket={ticket} key={ticket.id} />);
