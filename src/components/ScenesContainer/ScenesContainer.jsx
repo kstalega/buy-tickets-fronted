c362692e11e7e129f7b8244d2cf666ce5ac6bae1
Konrad Stalęga <konradstalega@gmail.com>
2018-06-04 20:28:18 +0200

    Bumping version up for deploy.

--- src/components/ScenesContainer/ScenesContainer.jsx
+++ src/components/ScenesContainer/ScenesContainer.jsx
@@ -32,7 +32,7 @@ const ScenesContainer = () =>
       <div class="col-sm-12">
         <div class="panel panel-default">
           <div class="panel-body">
-            System version: 0.75
+            System version: 0.77
           </div>
         </div>
       </div>
